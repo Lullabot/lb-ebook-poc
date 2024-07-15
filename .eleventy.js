3152e51c8d2b1749e363de831c26eea56751925c
Jerad Bitner <sirkitree@gmail.com>
2024-07-15 11:04:51 -0700

    adding font files from lullabot.com
    - also utilize the font faces
    - ensure that chapter numbers have a 0 prepended
    - print chapter number at the top of each chapter, according to designs

--- .eleventy.js
+++ .eleventy.js
@@ -40,6 +40,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("src/ebooks/**/*.png");
   eleventyConfig.addPassthroughCopy("src/ebooks/**/*.jpg");
   eleventyConfig.addPassthroughCopy("src/ebooks/**/*.jpeg");
